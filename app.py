51de95303de445f9f086341df524f1b717bdf998
Mrigank Badola <badola3@gmail.com>
2020-10-05 03:04:21 +0530

    added amazon functionality (still pending)

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-'''import urllib.request
-from bs4 import BeautifulSoup
-
-query = 'mobiles'
-page = urllib.request.urlopen(f'http://flipkart.com/search?q={query}')
-soupy = BeautifulSoup(page.read(),'html.parser')
-container = soupy.find_all("a", {"class": "_31qSD5"}, href=True)
-for x in container:
-    print(x['href'])'''
 from bs4 import BeautifulSoup as soup
 import urllib
 import requests
 import pandas as pd
+import re
 
 from flask import Flask, render_template,  session, redirect, request
 from flask_cors import CORS,cross_origin
@@ -26,49 +18,76 @@ class DataCollection:
 		"Comment Heading": list(), 
 		"Comment": list()}
 
-	def get_final_data(self, commentbox=None, prodName=None, prod_price=None):
+	def get_final_data(self, commentbox=None, prodName=None, prod_price=None, vendor=None):
 		'''
 		this will append data gathered from comment box into data dictionary
 		'''
 		self.data["Product"].append(prodName)
+		print(prodName)
 		self.data["Price (INR)"].append(prod_price)
-		try:
-			self.data["Name"].append(commentbox.div.div.\
-				find_all('p', {'class': '_3LYOAd _3sxSiS'})[0].text)
-		except:
-			self.data["Name"].append('No Name')
+		print(prod_price)
+		if vendor == 'flipkart':
+			try:
+				self.data["Name"].append(commentbox.div.div.\
+					find_all('p', {'class': '_3LYOAd _3sxSiS'})[0].text)
+			except:
+				self.data["Name"].append('No Name')
 
-		try:
-			self.data["Rating"].append(commentbox.div.div.div.div.text)
-		except:
-			self.data["Rating"].append('No Rating')
+			try:
+				self.data["Rating"].append(commentbox.div.div.div.div.text)
+			except:
+				self.data["Rating"].append('No Rating')
 
-		try:
-			self.data["Comment Heading"].append(commentbox.div.div.div.p.text)
-		except:
-			self.data["Comment Heading"].append('No Comment Heading')
+			try:
+				self.data["Comment Heading"].append(commentbox.div.div.div.p.text)
+			except:
+				self.data["Comment Heading"].append('No Comment Heading')
 
-		try:
-			comtag = commentbox.div.div.find_all('div', {'class': ''})
-			self.data["Comment"].append(comtag[0].div.text)
-		except:
-			self.data["Comment"].append('')	
+			try:
+				comtag = commentbox.div.div.find_all('div', {'class': ''})
+				self.data["Comment"].append(comtag[0].div.text)
+			except:
+				self.data["Comment"].append('')	
+		
+		else:
+			try:
+				self.data["Name"].append(commentbox.div.a.div.next_sibling.text)
+			except:
+				self.data["Name"].append('No Name')
 
-	def get_main_HTML(self, base_URL=None, search_string=None):
-		'''
-		return main html page based on search string
-		'''
-		search_url = f"{base_URL}/search?q={search_string}"
+			try:
+				self.data["Rating"].append(commentbox.div.next_sibling.a.text)
+			except:
+				self.data["Rating"].append('No Rating')
+
+			try:
+				self.data["Comment Heading"].append(commentbox.div.next_sibling.a.next_sibling.next_sibling.text)
+			except:
+				self.data["Comment Heading"].append('No Comment Heading')
+
+			try:
+				comtag = commentbox.div.next_sibling.next_sibling.next_sibling.next_sibling.span.div.div
+				self.data["Comment"].append(comtag.text)
+			except:
+				self.data["Comment"].append('')
+
+
+	def get_main_HTML(self, base_URL=None, search_string=None, vendor=None):
+		if vendor == 'flipkart':
+			search_url = f"{base_URL}/search?q={search_string}"
+		else:
+			search_url = f"{base_URL}/s?k={search_string}"
+		print(search_url)
 		with urllib.request.urlopen(search_url) as url:
 			page = url.read()
 		return soup(page, "html.parser")
 
-	def get_product_name_links(self, flipkart_base=None, bigBoxes=None):
+	def get_product_name_links(self, base_URL=None, bigBoxes=None):
 		temp = []
 		for box in bigBoxes:
 			try:
-				temp.append((box.div.div.div.a.img['alt'],
-					flipkart_base + box.div.div.div.a["href"]))
+				temp.append((box.img['alt'],
+					base_URL + box["href"]))
 			except:
 				pass
 			
@@ -93,7 +112,11 @@ def homePage():
 def index():
 	if request.method == 'POST':
 		try:
-			base_URL = 'https://www.flipkart.com'
+			vendor = request.form['shop']
+			if vendor == 'flipkart':
+				base_URL = 'https://www.flipkart.com'
+			else:
+				base_URL = 'https://www.amazon.in'
 			
 			search_string = request.form['query']
 			
@@ -102,21 +125,32 @@ def index():
 
 			get_data = DataCollection()
 
-			flipkart_HTML = get_data.get_main_HTML(base_URL, search_string)
+			query_HTML = get_data.get_main_HTML(base_URL, search_string, vendor)
 
-			bigBoxes = flipkart_HTML.find_all("div", {"class":"bhgxx2 col-12-12"})
+			if vendor=='flipkart':
+				bigBoxes = query_HTML.find_all("a", {"href":re.compile(r"\/.+\/p\/.+qH=.+")})
+			else:
+				bigBoxes = query_HTML.find_all("a", {"class": "a-link-normal s-no-outline", 
+													'href': re.compile(r'\/.+\/dp\/.+?dchild=1')})
 
 			product_name_Links = get_data.get_product_name_links(base_URL, bigBoxes)
 
 			for prodName, productLink in product_name_Links[:4]:
 				for prod_HTML in get_data.get_prod_HTML(productLink):
 					try:
-						comment_boxes = prod_HTML.find_all('div', {'class': '_3nrCtb'})
+						if vendor == 'flipkart':
+							comment_boxes = prod_HTML.find_all('div', {'class': '_3nrCtb'})
+							prod_price = prod_HTML.find_all('div', {"class": "_1vC4OE _3qQ9m1"})[0].text
+							
+						else:
+							comment_boxes = prod_HTML.find_all('div', {'id':re.compile(r'customer_review-.+')})
+							container = prod_HTML.find_all('td', {"class": "a-span12"})
+							container = soup(str(container),'html.parser')
+							prod_price = container.find_all('span', {"id": "priceblock_ourprice"})[0].text
 
-						prod_price = prod_HTML.find_all('div', {"class": "_1vC4OE _3qQ9m1"})[0].text
-						prod_price = float((prod_price.replace("₹", "")).replace(",", ""))
+						prod_price = float((prod_price.replace("₹", "")).replace(",", "").replace(" ",""))
 						for commentbox in comment_boxes:
-							get_data.get_final_data(commentbox, prodName, prod_price)
+							get_data.get_final_data(commentbox, prodName, prod_price, vendor)
 							
 					except:
 						pass
